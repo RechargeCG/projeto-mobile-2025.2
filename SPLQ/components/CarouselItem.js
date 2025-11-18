28a07a84eacd957e4645e1c1769aca16fdae54f5
cefet <richardcoelhokta@gmail.com>
2025-11-18 07:41:31 -0300

    Devolvendo CarouselItem.js

---
+++ SPLQ/components/CarouselItem.js
@@ -0,0 +1,39 @@
+  import React from 'react';
+  import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+  import Animated from 'react-native-reanimated';
+
+  const CarouselItem = ({ item, index }) => {
+    // You can use useAnimatedStyle, useSharedValue, etc. here for item-specific animations
+    return (
+      <Animated.View style={styles.itemContainer}>
+        <TouchableOpacity style={{width: '100%'}}>
+          <Image source={item.source} style={styles.image} />
+          <Text style={styles.title}>{item.title}</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    itemContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    image: {
+      width: '100%',
+      height: '80%',
+      resizeMode: 'contain',
+      borderRadius: 8,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginTop: 10,
+      color: 'white',
+      textAlign: 'center'
+    },
+  });
+
+  export default CarouselItem;
